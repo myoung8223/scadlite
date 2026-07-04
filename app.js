f5b9a5ed1775e6fab73dbc3299d6d1da888e2095
myoung8223 <141894282+myoung8223@users.noreply.github.com>
2026-07-04 11:22:17 -0500

    Update app.js
    Build push to 269.

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // ---- BUILD VERSION CONTROLLER ----
-const BUILD_NUMBER = "295"; //  <-- Incremented for SVG Import Database & Grid Layout
+const BUILD_NUMBER = "296";
 
 import OpenSCAD from './libs/openscad.js';
 
